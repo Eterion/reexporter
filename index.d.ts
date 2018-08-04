2c52ef6a99a259f9e637c702d35eef4e4afdeeef
Eterion <Eterion@users.noreply.github.com>
2018-08-04 11:39:39 +0200

    Definitions

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,36 @@
-export default function(): void;
+interface Callback extends Fs {
+  content: string;
+  modules: Module[];
+}
+
+interface Fs {
+  action: 'add' | 'idle' | 'update' | 'remove';
+  message: string;
+}
+
+interface Module {
+  isRecursion?: boolean;
+  name: string;
+  path: string;
+}
+
+interface Options {
+  fileExtension?: string;
+  fileExtensionInPath?: boolean;
+  fileName?: string;
+  fileNameInPath?: boolean;
+  ignore?: (string | RegExp)[];
+  log?: boolean;
+  moduleExtension?: string;
+  moduleExtensionInPath?: boolean;
+  moduleTemplate?: string;
+  recursion?: boolean;
+  recursionTemplate?: string;
+  recursionTemplateExport?: string;
+  test?: boolean;
+}
+
+export default function(
+  paths?: string | string[],
+  options?: Options
+): Promise<Callback[]>;
